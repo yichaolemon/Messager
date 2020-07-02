7d9aef9fa10612b0351471bed9f4384d0cdbfbc8
yichaolemon <yichao.cheng@yale.edu>
2020-07-01 20:09:59 -0700

    fix indentation

--- Storage.java
+++ Storage.java
@@ -4,9 +4,9 @@ import java.net.InetAddress;
 
 
 public interface Storage {
-	public void storeMessage(Message message);
-	public Message loadMessage(UUID uuid);
-	// if block is true, this method waits until there are messages to return.
-	public List<Message> loadMessageSince(InetAddress dstAddr, long timestamp, boolean block);
+  public void storeMessage(Message message);
+  public Message loadMessage(UUID uuid);
+  // if block is true, this method waits until there are messages to return.
+  public List<Message> loadMessageSince(InetAddress dstAddr, long timestamp, boolean block);
 }
 
