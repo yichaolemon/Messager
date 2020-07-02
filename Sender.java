23f8d2e92c7555f8a1420bf5948a78aa7d5a31f8
Lee Danilek <lee.danilek@gmail.edu>
2020-07-01 22:48:09 -0700

    make sender have a nicer interface, but don't change server so it's broken

--- Sender.java
+++ Sender.java
@@ -8,15 +8,53 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/*
+
+# Protocol
+Generally the command separator is the newline,
+and within commands each component is separated by a pipe.
+Client can send these commands:
+login|[username]|[password]
+create group|[group id]|[username]|[username]|...
+
+# these are constructed by the client automatically while the user is just typing in messages.
+fetch|[group id]|[start timestamp]
+send|[group id]|[message]
+close|[group id]
+
+Server sends back
+message|[group id]|[message]
+
+ */
+
+/*
+Command line REPL
+login [username] [password]
+create group [group id] [username],[username],...
+enter group [group id]
+[message]
+exit
+*/
+
 public class Sender extends Thread {
-  private InetAddress dstInetAddr;
+  private boolean inRepl = true;
+  private int currentGroup;
   private StringBuilder msg;
   private final Lock lock = new ReentrantLock();
   private final Condition strNotEmpty = lock.newCondition();
   private Socket senderSkt;
+  private PrintWriter outputWriter;
   private final int SERVER_PORT = 5100;
 
   private class SReceiver extends Thread {
+    private int currentGroup;
+    public synchronized void setCurrentGroup(int currentGroup) {
+      this.currentGroup = currentGroup;
+    }
+    private synchronized int getCurrentGroup() {
+      return self.currentGroup;
+    }
+
     public void run() {
       Scanner inStream;
       try {
@@ -29,7 +67,12 @@ public class Sender extends Thread {
       try {
         while (inStream.hasNextLine()) {
           String nl = inStream.nextLine();
-          System.out.println(nl);
+          String[] components = nl.split("|", 3);
+          int groupReceived = Integer.parseInt(components[1]);
+          String messageReceived = components[2];
+          if (groupReceived == self.getCurrentGroup()) {
+            System.out.println(messageReceived);
+          }
         }
       } catch (Exception e) {
         e.printStackTrace();
@@ -49,7 +92,7 @@ public class Sender extends Thread {
     }
   }
 
-  public String getMsg() throws InterruptedException {
+  private String getMsg() throws InterruptedException {
     lock.lock();
     try {
       while (msg.length() == 0) {
@@ -64,20 +107,74 @@ public class Sender extends Thread {
   }
 
   public void run() {
-    while (true) {
-      // tries to keep a persistent connection with server
-      try {
-        PrintWriter msgStream = new PrintWriter(senderSkt.getOutputStream(), true /*auto flushing*/);
-        while (true) {
-          String msgString = this.getMsg();
-          msgStream.write(msgString);
-          msgStream.flush();
-        }
-      } catch (Exception e) {
-        continue;
+    // tries to keep a persistent connection with server
+    try {
+      while (true) {
+        this.handleInput(this.getMsg());
       }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
+
+  private void handleInput(String input) {
+    if inRepl {
+      handleCommand(input);
+    } else {
+      handleMessage(input);
+    }
+  }
+
+  private static final Pattern loginPattern = Pattern.compile("^login (\\w+) (\\S+)$");
+  private static final Pattern createGroupPattern = Pattern.compile("^create group (\\d+) (\\w+(,\\w+)*)$");
+  private static final Pattern enterGroupPattern = Pattern.compile("^enter group (\\d+)$");
+
+  private void handleCommand(String command) {
+    Matcher loginMatch = loginPattern.matcher(command);
+    if loginMatch.matches() {
+      String username = loginMatch.group(1);
+      String password = loginMatch.group(2);
+      outputWriter.printf("login|%s|%s\n", username, password);
+      return;
+    }
+    Matcher createGroupMatch = createGroupPattern.matcher(command);
+    if createGroupMatch.match() {
+      String groupId = createGroupMatch.group(1);
+      String usernameStr = createGroupMatch.group(2);
+      String[] usernames = usernameStr.split(",", usernameStr.length());
+      outputWriter.printf("create group|%d", groupId);
+      for (String username: usernames) {
+        outputWriter.printf("|%s", username);
+      }
+      outputWriter.printf("\n");
+      return;
+    }
+    Matcher enterGroupMatch = enterGroupPattern.matcher(command);
+    if enterGroupMatch.match() {
+      int groupId = Integer.parseInt(enterGroupMatch.group(1));
+      enterGroup(groupId);
+      return;
+    }
+    System.out.println("unrecognized command");
+  }
+
+  private void enterGroup(int groupId) {
+      inRepl = false;
+      currentGroup = groupId;
+      receiver.setCurrentGroup(groupId);
+      long timestamp = 0;  // TODO
+      outputWriter.printf("fetch|%d|%d\n", currentGroup, timestamp);
+  }
+
+  private void handleMessage(String msg) {
+    if msg.equals("exit") {
+      inRepl = true;
+      outputWriter.printf("close|%d\n", currentGroup);
+    }
+    outputWriter.printf("%d|%s\n", currentGroup, msgString);
+  }
+
+  private SReceiver receiver;
 
   public Sender(InetAddress dstInetAddr) {
     this.dstInetAddr = dstInetAddr;
@@ -85,11 +182,12 @@ public class Sender extends Thread {
     try {
       senderSkt = new Socket(dstInetAddr, SERVER_PORT);
       System.out.println("Established connection with server at "+senderSkt.getRemoteSocketAddress().toString());
+      outputWriter = new PrintWriter(senderSkt.getOutputStream(), true /*auto flushing*/);
     } catch (Exception e) {
       e.printStackTrace();
       return;
     }
-    SReceiver receiver = new SReceiver();
+    receiver = new SReceiver();
     receiver.start();
   }
 }
